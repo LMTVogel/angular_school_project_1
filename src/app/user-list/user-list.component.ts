29b5c3001466414bfcda5ddd3fff0b957d2d522a
Luuk Vogel <luukbird@gmail.com>
2022-11-19 17:15:45 +0100

    Users are now retrieved from local storage

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../shared/models/user.model';
+import { UserService } from '../shared/services/user/user.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss']
+})
+export class UserListComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.users = this.userService.getUsers();
+  }
+
+}
