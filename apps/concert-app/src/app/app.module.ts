b1c3e867f22fb34b8f030fd2cbc84921471449a5
Luuk Vogel <luukbird@gmail.com>
2022-11-26 22:02:00 +0100

    Made changes to the modules.

--- apps/concert-app/src/app/app.module.ts
+++ apps/concert-app/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from './services/user/user.service';
-import * as fromComponents from '.';
 
 @NgModule({
   imports: [CommonModule, NgModule],
-  declarations: [...fromComponents.components],
 })
 export class SharedModule {}
