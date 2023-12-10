a309f44760d6f2bb867ca75e921697c3b0b613a4
Luuk <lmt.vogel@student.avans.nl>
2023-12-10 17:18:03 +0100

    Cleaned up some comments and useless functions

--- libs/concert/src/lib/concert.service.ts
+++ libs/concert/src/lib/concert.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { Concert } from '@angular-concert-project/concert';
 import {map, Observable} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -10,39 +10,6 @@ import {HttpClient} from "@angular/common/http";
 export class ConcertService {
   private url = 'https://angularschoolproject1-production.up.railway.app/api/concerts';
   // private url = 'http://localhost:3333/api/concerts';
-  //
-  // concerts: Concert[] = [
-  //   { id: 0,
-  //     name: 'Queen + Adam Lambert',
-  //     startDate: new Date('05/05/2022'),
-  //     maxTickets: 10000,
-  //     minimumAge: 16,
-  //   },
-  //   { id: 1,
-  //     name: 'Billy Joel',
-  //     startDate: new Date('05/05/2022'),
-  //     maxTickets: 10000,
-  //     minimumAge: 16,
-  //   },
-  //   { id: 2,
-  //     name: 'Jon Bon Jovi',
-  //     startDate: new Date('05/05/2022'),
-  //     maxTickets: 10000,
-  //     minimumAge: 16,
-  //   },
-  //   { id: 3,
-  //     name: 'Elton John',
-  //     startDate: new Date('05/05/2022'),
-  //     maxTickets: 10000,
-  //     minimumAge: 16,
-  //   },
-  //   { id: 4,
-  //     name: 'George Michael',
-  //     startDate: new Date('05/05/2022'),
-  //     maxTickets: 10000,
-  //     minimumAge: 16,
-  //   },
-  // ];
 
   constructor(private httpClient: HttpClient) { }
 
--- libs/user/src/lib/user.service.ts
+++ libs/user/src/lib/user.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import {catchError, map, Observable, of} from "rxjs";
+import { map, Observable } from "rxjs";
 import { User } from "@angular-concert-project/user";
 import { AuthService } from "@angular-concert-project/auth-ui";
 
@@ -13,19 +13,6 @@ export class UserService {
   // private url = 'http://localhost:3333/api/users';
 
   constructor(private httpClient: HttpClient, private authService: AuthService) { }
-
-  users: User[] = [
-    { id: '1', name: 'Alice Smith', email: 'alice@example.com', bday: new Date('1990-01-01'), isAdmin: true },
-    { id: '2', name: 'Bob Johnson', email: 'bob@example.com', bday: new Date('1985-02-02'), isAdmin: false },
-    { id: '3', name: 'Charlie Davis', email: 'charlie@example.com', bday: new Date('1992-03-03'), isAdmin: false },
-    { id: '4', name: 'Diana Evans', email: 'diana@example.com', bday: new Date('1988-04-04'), isAdmin: true },
-    { id: '5', name: 'Ethan Harris', email: 'ethan@example.com', bday: new Date('1991-05-05'), isAdmin: false },
-    { id: '6', name: 'Fiona King', email: 'fiona@example.com', bday: new Date('1989-06-06'), isAdmin: false },
-    { id: '7', name: 'George Lee', email: 'george@example.com', bday: new Date('1993-07-07'), isAdmin: true },
-    { id: '8', name: 'Hannah Martin', email: 'hannah@example.com', bday: new Date('1987-08-08'), isAdmin: false },
-    { id: '9', name: 'Ian Clark', email: 'ian@example.com', bday: new Date('1995-09-09'), isAdmin: false },
-    { id: '10', name: 'Julia Young', email: 'julia@example.com', bday: new Date('1986-10-10'), isAdmin: true }
-  ];
 
   getAllUsers(): Observable<User[]> {
     return this.httpClient.get<User[]>(this.url).pipe(
@@ -54,38 +41,4 @@ export class UserService {
   deleteUser(id: string): Observable<any> {
     return this.httpClient.delete(this.url + '/' + id);
   }
-
-  // getAllUsers(): Observable<User[]> {
-  //   const token = this.getToken();
-  //   const headers = new HttpHeaders({
-  //     'Access-Control-Allow-Origin': '*',
-  //     Authorization: `${token}`,
-  //   });
-
-  //   return this.httpClient.get<User[]>(this.url, {
-  //     headers: headers,
-  //   });
-  // }
-
-  // getUserById(userId: string): Observable<User> {
-  //   console.log('get user profile');
-
-  //   return this.httpClient.get<User>(this.url + '/' + userId, {
-  //     headers: this.authService.constructHeader(),
-  //   });
-  // }
-
-  // getOrganisationsFromUser(): Observable<User> {
-  //   console.log('get organisations from user');
-
-  //   const token = this.getToken();
-  //   const headers = new HttpHeaders({
-  //     'Access-Control-Allow-Origin': '*',
-  //     Authorization: `${token}`,
-  //   });
-
-  //   return this.httpClient.get<User>(this.url + "/organisations", {
-  //     headers: headers,
-  //   });
-  // }
 }
